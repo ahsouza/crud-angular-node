610476c053c507e949369b5e413e8f4c76054742
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-17 13:55:55 -0300

    #ISSUE_11 Adicionando métodos para salvar pessoa

--- public/js/controllers/PessoaController.js
+++ public/js/controllers/PessoaController.js
@@ -1,3 +1,40 @@
-angular.module('crud-angular-nodejs').controller('PessoaController', function($scope, $routeParams) {
-	console.log($routeParams.pessoaId)
+angular.module('crud-angular-nodejs').controller('PessoaController', function($scope, $routeParams, Pessoa, $rootScope) {
+
+  Pessoa.query(function(pessoas) {
+    console.log("Lista de clientes")
+	$scope.pessoas = pessoas
+  })
+		
+  if($routeParams.pessoaId) {
+	console.log('Parâmetro ' + $routeParams.pessoaId)
+	
+	Pessoa.get({id: $routeParams.pessoaId}, 
+	  function(pessoa) {
+	    console.log(`Cliente encontrado: ${pessoa}`)
+		$scope.pessoa = pessoa
+      }, 
+	  function(erro) {
+	    console.log(erro)
+		$scope.mensagem = {texto: 'Cliente não existe. Cadastre um novo cliente!'}
+	  }
+	);
+
+  } else {
+	console.log("Cadastrar nova pessoa")
+	$scope.pessoa = new Pessoa()
+  }
+
+	$scope.salva = function() {
+		
+		$scope.pessoa.$save()
+		.then(function() {
+			$scope.mensagem = {texto: 'Salvo com sucesso'}
+			$scope.pessoa = new Pessoa()
+			$scope.$broadcast('cliente Salvo')
+		})
+		.catch(function(erro) {
+  			$scope.mensagem = {texto: 'Não foi possível salvar'}
+  		})
+	}
+
 })
