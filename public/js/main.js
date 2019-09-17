31a765b8b2420a743ccf091ef65f0270af7d0834
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-17 14:08:26 -0300

    #ISSUE_12 Adicionando métodos para conexão com banco de dados

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,22 @@
-angular.module('crud-angular-nodejs', ['ngRoute'])
-  .config(function($routeProvider) {
+angular.module('crud-angular-nodejs', ['ngRoute', 'ngResource'])
+  .config(function($routeProvider,  $httpProvider) {
 
   	$routeProvider.when('/pessoas', {
-  		templateUrl: 'partials/pessoas.html',
-  		controller: 'PessoasController'
+  	  templateUrl: 'partials/pessoas.html',
+  	  controller: 'PessoasController'
   	})
+
+
+	$routeProvider.when('/pessoa/:pessoaId', {
+      templateUrl: 'partials/pessoa.html',
+      controller: 'PessoaController'
+    })
+
+    $routeProvider.when('/pessoa', {
+      templateUrl: 'partials/pessoa.html',
+      controller: 'PessoaController'
+    })
+
+    $routeProvider.otherwise({redirectTo: '/pessoas'})
+
   })
