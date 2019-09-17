f54fbf6b0c20d32e4047824b2fb2b02b2609ffb1
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-17 13:42:49 -0300

    #ISSUE_10 Adicionando métodos para busca e remoção de pessoas cadastradas

--- public/js/controllers/PessoasController.js
+++ public/js/controllers/PessoasController.js
@@ -19,4 +19,31 @@ angular.module('crud-angular-nodejs').controller('PessoasController', function($
 	];
 
 	$scope.filtro= ''
+
+	$scope.mensagem = {texto: ''}
+  
+	function buscaPessoas() {
+	  Pessoa.query(
+	    function(pessoas) {
+	      $scope.pessoas = pessoas
+	      $scope.mensagem = {}
+	    },
+	    function(erro) {
+	      console.log(erro)
+	      $scope.mensagem = {texto: 'Não foi possível obter a lista'}
+	    }
+	  )
+	}
+	buscaPessoas()
+
+    $scope.remove = function(pessoa) {
+      Pessoa.delete({id: pessoa._id}, 
+        buscaPessoas, 
+        function(erro) {
+          $scope.mensagem = {texto: 'Não foi possível remover o cliente'};
+          console.log(erro)
+        }
+      )
+    }
+
 })
