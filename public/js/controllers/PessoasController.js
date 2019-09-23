be9c54f680fd50056807969292b6ae0e8fb26432
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-22 23:49:35 -0300

    #ISSUE_39 DEMO projeto finalizado com sucesso!

--- public/js/controllers/PessoasController.js
+++ public/js/controllers/PessoasController.js
@@ -1,22 +1,6 @@
 angular.module('crud-angular-node').controller('PessoasController', function(Pessoa, $scope) {
 
-	$scope.pessoas = [
-	  {
-		"_id": 1,
-		"nome": "Contato Angular 1",
-		"email": "cont1@empresa.com.br"
-	  },
-	  {
-		"_id": 2,
-		"nome": "Contato Angular 2",
-		"email": "cont2@empresa.com.br"
-	  },
-	  {
-		"_id": 3,
-		"nome": "Contato Angular 3",
-		"email": "cont3@empresa.com.br"
-	  }
-	];
+	$scope.pessoas = [];
 
 	$scope.filtro= ''
 
